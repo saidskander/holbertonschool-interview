4ec1c5c04518b161c4add150672520259f9a7580
saidskander <askander199h@outlook.com>
2021-10-28 21:03:53 +0200

    Done

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Change comes from within """
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed
+    to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+
+    newcointotal = total + 1
+    amount = {0: 0}
+
+    for x in range(1, total + 1):
+        amount[x] = newcointotal
+
+        for coin in coins:
+            current = x - coin
+            if current < 0:
+                continue
+
+            amount[x] = min(amount[current] + 1, amount[x])
+
+    if amount[total] == total + 1:
+        return -1
+
+    return amount[total]
