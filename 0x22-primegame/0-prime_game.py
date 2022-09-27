50c4cf21346bdc772412c6652e4123f3ea300168
saidskander <askander199h@outlook.com>
2022-09-27 07:33:56 +0200

    Prime winner

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+methods:
+https://www.tutorialspoint.com/
+different-methods-to-find-prime-number-in-python-program
+"""
+def isWinner(x, nums):
+    """Prime Game"""
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    filter = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not filter[i]:
+            continue
+        for range_numbers in range(i * i, n + 1, i):
+            filter[range_numbers] = False
+
+    filter[0] = filter[1] = False
+    l = 0
+    for i in range(len(filter)):
+        if filter[i]:
+            l += 1
+        filter[i] = l
+
+    place = 0
+    for n in nums:
+        place += filter[n] % 2 == 1
+    if place * 2 == len(nums):
+        return None
+    if place * 2 > len(nums):
+        return "Maria"
+    return "Ben"
