c9edf09360ed94826efdc2caf8046cde44f2c397
saidskander <askander199h@outlook.com>
2022-09-27 20:46:40 +0200

    Island Perimeter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
+""" Island perimeter """
+
+
 def island_perimeter(grid):
     """
-    returns the perimeter of the island described in grid
+    Returns the perimeter of the island described in grid
     """
     p = 0
-    """ length: number of lines"""
+    """ Length: number of lines"""
     length = len(grid)
-    """ width: number of columns """
+    """ Width: number of columns """
     width = len(grid[0])
     for c in range(length):
         for i in range(width):
-            """ if there is 1 and no 1 above it """
+            """ If there is 1 and no 1 above it """
             if grid[c][i] == 1 and ((c > 0 and grid[c - 1][i] != 1) or c == 0):
                 p += 1
-            """ if there is 1 and no 1 below it """
+            """ If there is 1 and no 1 below it """
             if grid[c][i] == 1 and ((c + 1 != length and grid[c + 1][i] != 1)
                                     or c + 1 == length):
                 p += 1
-            """ if there is 1 and no 1 on the left """
+            """ If there is 1 and no 1 on the left """
             if grid[c][i] == 1 and ((i > 0 and grid[c][i - 1] != 1) or i == 0):
                 p += 1
-            """ if there is 1 and no 1 on the right """
+            """ If there is 1 and no 1 on the right """
             if grid[c][i] == 1 and ((i + 1 != width and grid[c][i + 1] != 1)
                                     or i + 1 == width):
                 p += 1
